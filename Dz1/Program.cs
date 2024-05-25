b44804940123e5f7347783610f8ce3939040a0c3
Evgeni Kirichenko <zhenya_kirichenko_2000@mail.ru>
2024-05-25 15:02:18 +0300

    Контрольная работа по рекурсии

---
+++ Dz1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System; 
+ 
+class Program 
+{ 
+    static void Main() 
+    { 
+        Console.Write("Введите значение M: "); 
+        int M = Convert.ToInt32(Console.ReadLine()); 
+ 
+        Console.Write("Введите значение N: "); 
+        int N = Convert.ToInt32(Console.ReadLine()); 
+ 
+        PrintNaturalNumbers(M, N); 
+    } 
+ 
+    static void PrintNaturalNumbers(int N, int M) 
+    { 
+        if (N <= M) 
+        {
+            if (N > 0)
+            {
+            Console.Write(N + " "); 
+            PrintNaturalNumbers(N + 1, M);
+            }
+            else PrintNaturalNumbers(N + 1, M );
+        } 
+    } 
+}
---
+++ Dz2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System; 
+ 
+class Program 
+{ 
+    static void Main() 
+    { 
+        int[] myArray = { 1, 99, 4, 3, 7 }; 
+ 
+        Console.WriteLine("Элементы массива в обратном порядке:"); 
+        PrintArrayReverse(myArray, myArray.Length - 1); 
+    } 
+ 
+    static void PrintArrayReverse(int[] arr, int index) 
+    { 
+        if (index >= 0) 
+        { 
+            Console.Write(arr[index] + " "); 
+            PrintArrayReverse(arr, index - 1); 
+        } 
+    } 
+}
